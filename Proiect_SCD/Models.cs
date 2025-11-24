f0a6cc2d711beabd0723e03aa0683de6a158a8c9
Opris Rares <oprisrares78@gmail.com>
2025-11-24 13:11:23 +0200

    First Commit

---
+++ Proiect_SCD/Models.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace SpringApiClient
+{
+    public class Country
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public long? Population { get; set; }
+        public long? Gdp { get; set; }
+    }
+
+    public class Continent
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public List<Country> Countries { get; set; }
+    }
+}
